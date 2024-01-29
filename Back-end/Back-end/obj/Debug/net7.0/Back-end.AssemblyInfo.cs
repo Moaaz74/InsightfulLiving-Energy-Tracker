1a7cf2b1060c7ec2072acc31a19a8e9c35858428
mohamedessamanwar <mohamed.essam.anwar2001@gmail.com>
2024-01-29 21:14:23 +0200

    add fire-detection-using-vgg16

--- Back-end/Back-end/obj/Debug/net7.0/Back-end.AssemblyInfo.cs
+++ Back-end/Back-end/obj/Debug/net7.0/Back-end.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddf1208f744ee9b80bfaaf096dc428cade563a20")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
