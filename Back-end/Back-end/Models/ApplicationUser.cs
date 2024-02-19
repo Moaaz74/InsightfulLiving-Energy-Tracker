b12eb6d8251919607752a3680e47a88f670cb0a3
Moaaz Mostafa <91892701+Moaaz74@users.noreply.github.com>
2024-02-19 16:33:03 +0200

    Update ApplicationUser.cs

--- Back-end/Back-end/Models/ApplicationUser.cs
+++ Back-end/Back-end/Models/ApplicationUser.cs
@@ -6,32 +6,6 @@ namespace Back_end.Models
 {
     public class ApplicationUser : IdentityUser
     {
-        public int Id { get; set; }
-
-        [DataType(DataType.Password)]
-        public string Password { get; set; }
-
-        [DataType(DataType.EmailAddress)]
-        public string Email { get; set; }
-
-        [Range(20, 60)]
-        public int Age { get; set; }
-
-        [Required]
-        [RegularExpression(@"[a-zA-Z]+", ErrorMessage = "Name Must be only characters")]
-        [Display(Name = "User Name")]
-        public string Name { get; set; }
-
-        public Gender Gender { get; set; }
-
-        public bool IsActive { get; set; }
-
-        [ForeignKey("Home")]
-        public int HomeId { get; set; }
-
-
+       
     }
-
-    public enum Gender
-    { Male = 0, Femal = 1 }
 }
