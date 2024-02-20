f3e0601aefc5ea87a57366bd732f9cfbb19a476f
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-02-20 15:16:56 +0200

    added the DTOs of the cassandra tables

---
+++ Back-end/Back-end/DTOs/Cassandra_quries/ApplianceDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Back_end.DTOs.Cassandra_quries
+{
+    public class ApplianceDto
+    {
+        public int ApplianceId { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public double ApplianceConsumption { get; set; }
+        public string EnergyType { get; set; }
+    }
+}
---
+++ Back-end/Back-end/DTOs/Cassandra_quries/Room_OverallDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Back_end.DTOs.Cassandra_quries
+{
+    public class Room_OverallDto
+    {
+        public int RoomId { get; set; }
+        public string Start { get; set; }
+        public string End { get; set; }
+        public double RoomConsumption { get; set; }
+        public string EnergyType { get; set; }
+    }
+}
