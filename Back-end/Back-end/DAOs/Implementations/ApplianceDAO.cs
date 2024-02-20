4dc2ab1e4f45a73daf57bc4b93fe3882b218f94d
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-02-20 15:12:43 +0200

    Added verything related with appliance tabele in cassandra

---
+++ Back-end/Back-end/DAOs/Implementations/ApplianceDAO.cs
@@ -0,0 +1,44 @@
+﻿using Back_end.DAOs.Interfaces;
+using Back_end.Models;
+using Cassandra.Mapping;
+
+namespace Back_end.DAOs.Implementations
+{
+    public class ApplianceDAO : IApplianceDAO
+    {
+        protected readonly Cassandra.ISession session;
+        protected readonly IMapper mapper;
+        private readonly IConfiguration _configuration;
+
+        public ApplianceDAO(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            ICassandraDAO cassandraDAO = new CassandraDAO(_configuration.GetValue<string>("CassandraConfiguration:cassandraNodes", "127.0.0.1"), _configuration.GetValue<string>("CassandraConfiguration:Keyspace", "big_data"));
+            session = cassandraDAO.GetSession();
+
+            mapper = new Mapper(session);
+
+        }
+
+        public async Task<IEnumerable<Appliance>> getAppliance()
+        {
+
+            string cql = "SELECT * FROM Appliance ;";
+            try
+            {
+
+                return await mapper.FetchAsync<Appliance>(cql);
+
+            }
+
+            catch (Exception ex)
+            {
+                // Handle any exceptions that occurred during query execution
+                Console.WriteLine($"Error executing query: {ex.Message}");
+                return null;
+            }
+        }
+    }
+
+}
+
---
+++ Back-end/Back-end/DAOs/Interfaces/IApplianceDAO.cs
@@ -0,0 +1,9 @@
+﻿using Back_end.Models;
+
+namespace Back_end.DAOs.Interfaces
+{
+    public interface IApplianceDAO
+    {
+        Task<IEnumerable<Appliance>> getAppliance();
+    }
+}
