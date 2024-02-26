fa7f0249e0da9c814dea631d77ee58c765268032
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-02-26 23:37:35 +0200

    connection with the backend is built

--- Frontend/project/src/Component/Login/Login.jsx
+++ Frontend/project/src/Component/Login/Login.jsx
@@ -11,30 +11,33 @@ export default function Login(value) {
 
   let {setUserToken} = useContext(userContext)
   let validationSchema = yup.object({
-    email: yup.string().email("E-mail is inValid").required("E-mail is required"),
-    password: yup.string().matches(/[A-Z][a-z0-9]{5,10}/ , "Password is inValid").required("Password is required"),
+    username:yup.string().matches(/^[a-zA-Z0-9_]{3,30}$/, "Username must be between 3 and 30 characters").required("Username is required"),
+    password: yup.string().matches(/[A-Z][a-zA-Z0-9@_]{5,20}/ , "Password is inValid").required("Password is required"),
   })
   let [Error,setError] = useState("")
   let [isLoading,setisLoadind] = useState(false)
   let navigate = useNavigate();
   async function submitRegister(value) {
     setisLoadind(true)
-    let {data} = await axios.post(`https://ecommerce.routemisr.com/api/v1/auth/signin` ,value)
+    let {data} = await axios.post(`http://localhost:62863/api/user/Login` ,value)
     .catch((err)=>{ 
       setisLoadind(false)
-      setError(err.response.data.message)
+      setError(err.response.data.title)
+      console.log("error : ", data);
     })
-    if(data.message === "success" ){
+   
+    if(data){
       setisLoadind(false)
       localStorage.setItem("userToken" , data.token)
       setUserToken(data.token)
         navigate("/")
     }
+    
   }
 
   let formik= useFormik({
     initialValues:{
-      email:'',
+      username:'',
       password:'',
     },validationSchema,
     onSubmit: submitRegister
@@ -49,9 +52,9 @@ export default function Login(value) {
 
     <form onSubmit={formik.handleSubmit}>
 
-      <label htmlFor="email" className='mb-2'>Email :</label>
-      <input value={formik.values.email} onChange={formik.handleChange} onBlur={formik.handleBlur} name='email' className='form-control mb-2' id='email' type="email" />
-      {formik.errors.email && formik.touched.email ? <div className="alert alert-danger mt-2 mb-3 p-2">{formik.errors.email}</div> : null }
+      <label htmlFor="username" className='mb-2'>UserName :</label>
+      <input value={formik.values.username} onChange={formik.handleChange} onBlur={formik.handleBlur} name='username' className='form-control mb-2' id='username' type="username" />
+      {formik.errors.username && formik.touched.username ? <div className="alert alert-danger mt-2 mb-3 p-2">{formik.errors.username}</div> : null }
 
 
       <label htmlFor="password" className='mb-2'>Password :</label>
@@ -61,7 +64,6 @@ export default function Login(value) {
       {!isLoading?
       <div className='d-flex align-items-center mx-2'>
       <button disabled={!(formik.isValid && formik.dirty)} type='submit' className='btn bg-main me-2 mt-2 text-white'>Login</button>
-      <Link className='btn' to={"/register"}>Register Now</Link>
       </div>
       :
       <button type='button' className='bg-main mt-3 text-white none rounded-3'>
