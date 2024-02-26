195d750c1189949047ad903b506061117303a47e
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-02-26 23:40:45 +0200

    deleted anything related to register

--- Frontend/project/src/App.js
+++ Frontend/project/src/App.js
@@ -2,7 +2,6 @@ import './App.css';
 import { createBrowserRouter , RouterProvider } from 'react-router-dom'
 import Home from "./Component/Home/Home"
 import Login from './Component/Login/Login';
-import Register from './Component/Register/Register';
 import Layout from './Component/Layout/Layout';
 import { useContext, useEffect } from 'react';
 import  { userContext } from './Context/UserContext';
@@ -27,7 +26,7 @@ function App() {
       
     ]},
     {path:"login" , element: <Login/>},
-      {path:"register" , element:<Register/>},
+      
   ])
 
   return <RouterProvider router={routers}></RouterProvider>
