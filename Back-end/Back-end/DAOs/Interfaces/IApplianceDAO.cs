9bff3e7bc6b3e086c9706df6f94e7ef7cf69823c
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-03-02 16:26:10 +0200

    Added last query request of table appliance

--- Back-end/Back-end/DAOs/Interfaces/IApplianceDAO.cs
+++ Back-end/Back-end/DAOs/Interfaces/IApplianceDAO.cs
@@ -6,7 +6,7 @@ namespace Back_end.DAOs.Interfaces
 {
     public interface IApplianceDAO
     {
-        Task<IEnumerable<Appliance>> getAppliance();
+        Task<Appliance> getLastAppliance(int applianceid);
         Task<IEnumerable<String>> getApplianceStartDates(string energytype, int applianceid);
         Task<IEnumerable<String>> getApplianceEndDates(ApplianceS_DateDto s_DateDto, int applianceid);
         Task<IEnumerable<Double>> getApplianceconsumption(ApplianceDatesDto datesDto, int applianceid);
