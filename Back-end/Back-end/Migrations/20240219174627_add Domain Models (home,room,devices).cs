ddb4173eae13b5002c874787d80d3ed7ecbd3dca
Moataz-Nasr <moataznasrmoatazzzz163@gamil.com>
2024-04-15 00:32:42 +0200

    modifed the migrations

--- Back-end/Back-end/Migrations/20240219174627_add Domain Models (home,room,devices).cs
+++ Back-end/Back-end/Migrations/20240219174627_add Domain Models (home,room,devices).cs
@@ -15,7 +15,7 @@ namespace Back_end.Migrations
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
-                        .Annotation("SqlServer:Identity", "1, 1"),
+                        .Annotation("SqlServer:Identity", "268, 1"),
                     UserId = table.Column<string>(type: "nvarchar(450)", nullable: false),
                     NumberOfRooms = table.Column<int>(type: "int", nullable: false),
                     IsDeleted = table.Column<bool>(type: "bit", nullable: false)
