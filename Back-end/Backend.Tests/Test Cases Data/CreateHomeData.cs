59f9a349a8d7738018e31996e6e08aa35885bd5b
Moaaz Mostafa <moaazmostafa7474@gmail.com>
2024-06-04 01:02:14 +0300

    Added first test cases of Home Module

---
+++ Back-end/Backend.Tests/Test Cases Data/CreateHomeData.cs
@@ -0,0 +1,22 @@
+﻿using Back_end.DTOS.Home;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Backend.Tests.Test_Cases_Data
+{
+    public class CreateHomeData
+    {
+        public string Name { get; set; }
+        public string NumberOfRooms { get; set; }
+        public string UserId {  get; set; }
+        public ExpectedResult Expected { get; set; }
+    }
+
+    public class ExpectedResult
+    {
+        public int StatusCode { get; set; }
+    }
+}
